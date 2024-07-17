94e73d5eb17554db86d7a077924577dfd3063ce8
Marco Enrico <mvalviar@gmail.com>
2024-07-17 22:41:05 +0800

    Add firebase hosting

--- src/Hit.tsx
+++ src/Hit.tsx
@@ -1,28 +1,10 @@
 import parse from 'html-react-parser';
-import { useEffect, useRef } from 'react';
 
-export const Hit = ({ hit }) => {
-  const ref = useRef();
+export const Hit = ({ hit }: {hit:{ entry: string;};}) => {
   const entry = parse(hit.entry);
-//   useEffect(() => {
-// 	// Correct method to access the DOM element is ref.current
-// 	const element = ref.current;
-// 	if (element) {
-// 		const clickHandler = (e) => {
-// 			const newUrl = e.target.href
-// 			history.replaceState(null, '', newUrl);
-// 			e.preventDefault();
-// 		};
-// 		element.addEventListener('click', clickHandler);
-
-// 		return () => {
-// 			element.removeEventListener('click', clickHandler);
-// 		};
-// 	}
-//   }, [ref]);
   return (
     <article>
-      <div ref={ref} className="hit-id">
+      <div className="hit-id">
 			<div className="hit-entry">
 			{entry}
 			</div>
--- src/Search.tsx
+++ src/Search.tsx
@@ -1,22 +1,14 @@
-import { useLayoutEffect, useState } from 'react';
 import algoliasearch from "algoliasearch/lite";
 import "instantsearch.css/themes/satellite.css";
-import { Hits, InstantSearch, SearchBox, Configure, Pagination } from "react-instantsearch";
+import { useInstantSearch, InstantSearch, SearchBox, Configure, InfiniteHits, } from "react-instantsearch";
 
 import { Hit } from "./Hit";
 
 const searchClient = algoliasearch("D3FGLXXB5K", "82284a40fadf34dccdcc9cfacb893d27");
 
-export const Search = () => {
-  const [query, setQuery] = useState('');
-  useLayoutEffect(() => {
-    const match = window.location.href.match(/salita\/(\w+)(#\w+)?/);
-    if (match) {
-      console.log(match);
-      setQuery(match[1]);
-    }
+const query = decodeURIComponent(window.location.href).match(/\/salita\/([^#]+)/)?.[1] || '';
 
-  }, []);
+export const Search = () => {
   return (
     <InstantSearch
       searchClient={searchClient}
@@ -27,11 +19,48 @@ export const Search = () => {
         },
       }}
     >
-      <Configure hitsPerPage={20} />
+      <Configure hitsPerPage={100} />
       <div className="ais-InstantSearch">
-        <SearchBox />
-        <Hits hitComponent={Hit} />
+        <SearchBox placeholder='Hanapin sa Diksiyonaryong Filipino' />
+        <NoResultsBoundary fallback={<NoResults />}>
+          <InfiniteHits hitComponent={Hit} showPrevious={false} />
+        </NoResultsBoundary>
       </div>
     </InstantSearch>
   );
 };
+
+type NoResultsBoundaryProps = {
+  children: React.ReactNode;
+  fallback: React.ReactNode;
+};
+
+
+function NoResultsBoundary({ children, fallback }: NoResultsBoundaryProps) {
+  const { results } = useInstantSearch();
+
+  // The `__isArtificial` flag makes sure not to display the No Results message
+  // when no hits have been returned.
+  if (!results.__isArtificial && results.nbHits === 0) {
+    return (
+      <>
+        {fallback}
+        <div hidden>{children}</div>
+      </>
+    );
+  }
+
+  return children;
+}
+
+function NoResults() {
+  const { indexUiState } = useInstantSearch();
+
+  return (
+    <div>
+      <p>
+        Walang resulta para sa<q>{indexUiState.query}</q>.
+      </p>
+    </div>
+  );
+}
